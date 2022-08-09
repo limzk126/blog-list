3e409fcfde71236bc1870114754c9de163e4d6cc
puzzlehead <dylanlim12698@gmail.com>
2022-08-09 17:27:40 +0800

    Upload 4.13 to 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,7 +8,7 @@ blogRouter.get('/', (request, response) => {
 });
 
 blogRouter.post('/', (request, response) => {
-  if ((request.body.title == undefined || request.body.url == undefined)) {
+  if (request.body.title == undefined || request.body.url == undefined) {
     return response.status(400).json({
       error: 'title and url cannot be missing!',
     });
@@ -21,6 +21,23 @@ blogRouter.post('/', (request, response) => {
   blog.save().then((result) => {
     response.status(201).json(result);
   });
+});
+
+blogRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id);
+
+  if (blog) {
+    return response.status(204).end();
+  }
+});
+
+blogRouter.put('/:id', async (request, response) => {
+  console.log(request.body);
+  const blog = await Blog.findByIdAndUpdate(request.params.id, request.body, {
+    new: true,
+  });
+
+  return response.status(200).json(blog);
 });
 
 module.exports = blogRouter;
--- tests/blog-list.test.js
+++ tests/blog-list.test.js
@@ -4,6 +4,7 @@ const helper = require('./test_helper');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
+const { response } = require('../app');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -25,55 +26,132 @@ test('All blogs have the id property', async () => {
   response.body.forEach((blog) => expect(blog.id).toBeDefined());
 });
 
-test('New blog created is successfully', async () => {
-  const newBlog = {
-    title: 'Top 10 anime betrayals.',
-    author: 'Ruka Inaba',
-    url: 'http://kissanime.com.jp',
-    likes: 113,
-  };
+describe('Adding new blogs', () => {
+  test('New blog created is successfully', async () => {
+    const newBlog = {
+      title: 'Top 10 anime betrayals.',
+      author: 'Ruka Inaba',
+      url: 'http://kissanime.com.jp',
+      likes: 113,
+    };
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .set('Accept', 'application/json')
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Accept', 'application/json')
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  const updatedBlogList = (await api.get('/api/blogs')).body;
-  expect(updatedBlogList).toHaveLength(helper.listWithMultipleBlogs.length + 1);
+    const updatedBlogList = (await api.get('/api/blogs')).body;
+    expect(updatedBlogList).toHaveLength(
+      helper.listWithMultipleBlogs.length + 1
+    );
 
-  const titles = updatedBlogList.map((blog) => blog.title);
-  expect(titles).toContain('Top 10 anime betrayals.');
+    const titles = updatedBlogList.map((blog) => blog.title);
+    expect(titles).toContain('Top 10 anime betrayals.');
+  });
+
+  test('Likes default to zero if likes property missing', async () => {
+    const blog = {
+      ...helper.listWithOneBlog[0],
+    };
+    delete blog.likes;
+
+    const response = await api
+      .post('/api/blogs')
+      .send(blog)
+      .set('Accept', 'application/json')
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.likes).toBe(0);
+  });
+
+  test('Bad request is returned if title or url is missing', async () => {
+    const blog = {
+      ...helper.listWithOneBlog[0],
+    };
+    delete blog.title;
+    delete blog.url;
+
+    await api
+      .post('/api/blogs')
+      .send(blog)
+      .set('Accept', 'application/json')
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  });
 });
 
-test('Likes default to zero if likes property missing', async () => {
-  const blog = {
-    ...helper.listWithOneBlog[0],
-  };
-  delete blog.likes;
+describe('Deleting blogs', () => {
+  test('Deletes blog successfully', async () => {
+    const response = await api.get('/api/blogs');
 
-  const response = await api
-    .post('/api/blogs')
-    .send(blog)
-    .set('Accept', 'application/json')
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const id = response.body[0].id;
 
-  expect(response.body.likes).toBe(0);
+    await api.delete(`/api/blogs/${id}`).expect(204);
+
+    const responseAfterDel = await api.get('/api/blogs');
+
+    const ids = responseAfterDel.body.map((blog) => blog.id);
+
+    expect(ids).not.toContain(id);
+  });
+
+  test('Deleting non-existent blog returns 400', async () => {
+    await api.delete(`/api/blogs/wrongId`).expect(400);
+  });
+
+  test('Missing id parameter returns 404', async () => {
+    await api.delete(`/api/blogs/`).expect(404);
+  });
 });
 
-test('Bad request is returned if title or url is missing', async () => {
-  const blog = {
-    ...helper.listWithOneBlog[0],
-  };
-  delete blog.title;
-  delete blog.url;
+describe('Updating blogs', () => {
+  test('Update successful', async () => {
+    const blogs = (await api.get('/api/blogs')).body;
+    const blog = blogs[0];
+    const toUpdate = {
+      title: 'Top 10 anime betrayals',
+    };
+    const updatedBlog = {
+      ...blog,
+      title: 'Top 10 anime betrayals',
+    };
 
-  await api
-    .post('/api/blogs')
-    .send(blog)
-    .set('Accept', 'application/json')
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
+    const response = await api
+      .put(`/api/blogs/${blog.id}`)
+      .send(toUpdate)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toEqual(updatedBlog);
+  });
+
+  test('Updating non-existent properties return original blog', async () => {
+    const blogs = (await api.get('/api/blogs')).body;
+    const blog = blogs[0];
+    const toUpdate = {
+      titles: 'Nonsense',
+      irl: 'www.wrong.com',
+    };
+
+    const response = await api
+      .put(`/api/blogs/${blog.id}`)
+      .send(toUpdate)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toEqual(blog);
+  });
+
+  test('Updating non-existent blog returns 400', async () => {
+    await api.delete(`/api/blogs/wrongId`).expect(400);
+  });
+
+  test('Missing id parameter returns 404', async () => {
+    await api.delete(`/api/blogs/`).expect(404);
+  });
 });
